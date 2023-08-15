4ba66e5c8ada5c462988dd25039405ccd37720d6
Unknown <unknown@example.com>
2023-08-15 10:41:56 -0300

    ajste

--- projetin/src/main/java/visao/Telagalo.java
+++ projetin/src/main/java/visao/Telagalo.java
@@ -1,15 +1,20 @@
 package visao;
 
+import java.util.ArrayList;
+
+import control.galosDAO;
 import modelo.Galos;
 
 public class Telagalo {
 
 	public static void main(String[] args) {
-		Galos dao = new Galos();
+		galosDAO dao = new galosDAO();
 		
+		ArrayList<Galos> galons = dao.listar();
 		
-		
-		
+		for (Galos galos : galons) {
+			
+		}
 		
 	}
 	
