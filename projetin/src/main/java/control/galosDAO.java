5bcd8d522f61e78c15a87520a0b689c6f6372bd0
Unknown <unknown@example.com>
2023-08-15 10:20:05 -0300

    galons

--- projetin/src/main/java/control/galosDAO.java
+++ projetin/src/main/java/control/galosDAO.java
@@ -2,6 +2,7 @@ package control;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -40,16 +41,40 @@ public class galosDAO {
 	
 	public ArrayList<Galos> listar(){
 		
-Conexao c = Conexao.getInstancia();
+		Conexao c = Conexao.getInstancia();
 		
 		Connection cn = c.conectar();
 		
-		ArrayList<Galos> galon = new ArrayList(); 
+		ArrayList<Galos> galons = new ArrayList(); 
 		
-		String query = ")";
+		String query = "SELECT * FROM GALOS";
 				
+		try {
+			PreparedStatement ps = cn.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				int idGalo = rs.getInt("id_galo");
+				
+				String raca = rs.getString("raca_galo");
+				
+				int power = rs.getInt("poder_de_combate");
+						
+				Galos g = new Galos();
+				g.setIdGalo(idGalo);
+				g.setRaca(raca);
+				g.setPower(power);
+				
+				galons.add(g);
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
 		
-		return null;
+		c.fecharConexao()
+		
+		return galons;
 	}
 	
 	
