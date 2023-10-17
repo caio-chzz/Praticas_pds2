89c7cffda5a1602de24dd96035de41738d30e160
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-10-17 08:41:14 -0300

    asa

--- projetin/src/main/java/test/GaloDaoTest.java
+++ projetin/src/main/java/test/GaloDaoTest.java
@@ -1,43 +1,33 @@
 package test;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class GaloDaoTest {
-    
 
-@Test
-public void testegaloinsere(){
-    
-}
+    @Test
+    public void testegaloinsere() {
 
+    }
 
-@Test
-public void testegalolista(){
-    
-}
+    @Test
+    public void testegalolista() {
 
+    }
 
-@Test
-public void testegaloatualiza(){
-    
-}
+    @Test
+    public void testegaloatualiza() {
 
+    }
 
+    @Test
+    public void testegaloexclui() {
 
-@Test
-public void testegaloexclui(){
-    
-}
+    }
 
+    @Test
+    public void testegaloprocura() {
 
-@Test
-public void testegaloprocura(){
-    
-}
-
-
-
+    }
 
 }
-
