840a1a46d6b9bbbbb5c120215410272edeaf8ffc
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-10-24 08:31:50 -0300

    testeone

--- projetin/src/main/java/test/GaloDaoTest.java
+++ projetin/src/main/java/test/GaloDaoTest.java
@@ -1,16 +1,37 @@
 package test;
 
 import org.junit.Test;
+
+import control.GaloDAO;
+import modelo.Galos;
+
 import static org.junit.Assert.assertTrue;
 
 public class GaloDaoTest {
 
     @Test
     public void testegaloinsere() {
+         
+        GaloDAO galoDAO = new GaloDAO();
 
+        
+        Galos novoGalo = new Galos();
+        novoGalo.setIdGalo(1);
+        novoGalo.setRaca("GaloTeste");
+        novoGalo.setPower(100);
+        novoGalo.setName("GaloTesteNome");
+        novoGalo.setLife(50);
+
+     
+        boolean insercaoSucesso = galoDAO.inserir(novoGalo);
+
+      
+        assertTrue(insercaoSucesso);
+        assertTrue(!insercaoSucesso);
+          
     }
 
-    @Test
+  /*   @Test
     public void testegalolista() {
 
     }
@@ -29,5 +50,5 @@ public class GaloDaoTest {
     public void testegaloprocura() {
 
     }
-
+*/
 }
